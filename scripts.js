4ae0b6a9cf8eaac4b2fb6325f3729df0f16778b8
lesegomatshego <l.ematshego12@gmail.com>
2023-03-28 09:32:44 +0200

    styled the javascript

---
+++ scripts.js
@@ -0,0 +1,7 @@
+JAVA
+const MAX_NUMBER = 15;
+const MIN_NUMBER = -5;
+const STEP_AMOUNT = 5;
+const number = document.querySelector('[data-key="number"]');
+const subtract = document.querySelector('[data-key="subtract"]');
+const add = document.querySelector('[data-key="add"]');
