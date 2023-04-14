b8d09d6736c98409bcfee15905ee269c5d6ade37
lesegomatshego <l.ematshego12@gmail.com>
2023-04-14 15:46:14 +0200

    fixed th errors

--- IWA-12/script.js
+++ IWA-12/script.js
@@ -1,3 +1,4 @@
+// scripts.js
 const STATUS_MAP = {
     shelf: {
         color: 'green',
@@ -24,38 +25,39 @@ const STATUS_MAP = {
         canCheckIn: true,
     }
 }
-
-// Edit below line 
-
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
-
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
-
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
-
-checkin.0.color = none
-status.0.style.color = STATUS_MAP.status.color
-reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
-
-checkin.1.color = none
-status.1.style.color = STATUS_MAP.status.color
-reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
-
-checkin.2.color = none
-status.2.style.color = STATUS_MAP.status.color
-reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+// Edit below line
+const book1 = {
+status: document.querySelector('#book1 .status'),
+reserve: document.querySelector('#book1 .reserve'),
+checkout: document.querySelector('#book1 .checkout'),
+checkin: document.querySelector('#book1 .checkin')
+}
+const book2 = {
+status: document.querySelector('#book2 .status'),
+reserve: document.querySelector('#book2 .reserve'),
+checkout: document.querySelector('#book2 .checkout'),
+checkin: document.querySelector('#book2 .checkin')
+}
+const book3 = {
+    status: document.querySelector('#book3 .status'),
+    reserve: document.querySelector('#book3 .reserve'),
+    checkout: document.querySelector('#book3 .checkout'),
+    checkin: document.querySelector('#book3 .checkin')
+    }
+ function updateBookStatus(book) {
+    const status = book.status.textContent.trim()
+    const statusInfo = STATUS_MAP[status]
+    book.status.style.color = statusInfo.color
+    book.reserve.disabled = !statusInfo.canReserve
+    book.checkout.disabled = !statusInfo.canCheckout
+    book.checkin.disabled = !statusInfo.canCheckIn
+}
+updateBookStatus(book1)
+updateBookStatus(book2)
+updateBookStatus(book3)
+const overdue = book1.checkin
+const reserveCheckout = overdue.style.color = ''
+const reserved = book2.checkin
+const reserveCheckin = reserved.style.color = ''
+const shelf = book3.checkin
+const checkIn = shelf.style.color = ''
