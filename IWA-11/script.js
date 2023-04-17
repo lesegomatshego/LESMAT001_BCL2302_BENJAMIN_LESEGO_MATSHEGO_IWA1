a6a860120358bf6d4c660fbcbb6e8c55c1490537
lesegomatshego <l.ematshego12@gmail.com>
2023-04-17 15:07:07 +0200

    linked correctly

--- IWA-11/script.js
+++ IWA-11/script.js
@@ -4,7 +4,8 @@ const donut1  = parseInt(root1.getAttribute('data-donuts'));   //used parseInt f
 const pancake1  = parseInt(root1.getAttribute('data-pancakes'));
 const status1  = root1.getAttribute('data-delivered') === "true" ? "Delivered" : "Pending";
 const one = document.querySelector('h2');
-                      //order 2 to order 1
+
+  //order 2 to order 1
 document.querySelector('dd').innerText = biscuit1
 document.querySelector('.donuts dd').innerText = donut1
 document.querySelector('.pancakes dd').innerText = pancake1
@@ -15,11 +16,13 @@ const donut2  = parseInt(root2.getAttribute('data-donuts'));
 const pancake2  = parseInt(root2.getAttribute('data-pancakes'));
 const status2  = root2.getAttribute('data-delivered') === "true" ? "Delivered" : "Pending";//added tenery operator (? :) is used to check the value of the 'data delivered' attribite and set the text content accordingly.
 //order 2
+
 document.querySelectorAll('.biscuits dd')[1].innerText = biscuit2
 document.querySelectorAll('.donuts dd')[1].innerText = donut2
 document.querySelectorAll('.pancakes dd')[1].innerText = pancake2
 document.querySelectorAll('.status dd')[1].innerText = status2
 //order 3
+
 const root3 = document.querySelector('[data-key="order3"]');
 const biscuit3 = parseInt(root3.getAttribute('data-biscuits'));
 const donut3  = parseInt(root3.getAttribute('data-donuts'));
